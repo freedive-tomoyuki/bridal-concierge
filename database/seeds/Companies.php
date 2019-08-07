7a55c2318506361319db41e2b376cff444c20573
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-08-07 13:49:38 +0900

    [修正]uddate seeds Data

--- database/seeds/Companies.php
+++ database/seeds/Companies.php
@@ -11,13 +11,19 @@ class Companies extends Seeder
      */
     public function run()
     {
+        DB::table('companies')->truncate();
     	if (DB::table('companies')->count() > 0){
     		return;
     	}
         DB::table('companies')->insert([
             ['name' => 'ニューアート', 'kana' => 'ニューアート'],
             ['name' => 'ブリリアンス+', 'kana' => 'ブリリアンス+'],
-            ['name' => 'トレセンテ', 'kana' => 'トレセンテ']
+            ['name' => 'トレセンテ', 'kana' => 'トレセンテ'],
+            ['name' => 'エクセルコダイヤモンド', 'kana' => 'エクセルコダイヤモンド'],
+            ['name' => 'アイプリモ', 'kana' => 'アイプリモ'],
+            ['name' => 'ラザール', 'kana' => 'ラザール'],
+            ['name' => 'ケイウノ', 'kana' => 'ケイウノ'],
+            ['name' => 'ジュエリーカマタ', 'kana' => 'ジュエリーカマタ'],
         ]);
     }
 }
